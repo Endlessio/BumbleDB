575466f8d06fd7f8e3994210470bf47d47822c2d
Endlessio <117010423@link.cuhk.edu.cn>
2021-11-15 21:00:38 -0500

    hash

--- pkg/hash/table.go
+++ pkg/hash/table.go
@@ -164,27 +164,31 @@ func (table *HashTable) Split(bucket *HashBucket, hash int64) error {
 func (table *HashTable) Insert(key int64, value int64) error {
 	/* SOLUTION {{{ */
 	// fmt.Println("hash/table/insert: key, value", key, value)
-	table.RLock()
+	// table.RLock()
+	table.WLock()
 	hash := Hasher(key, table.depth)
 	bucket, err := table.GetBucket(hash)
 	if err != nil {
-		table.RUnlock()
+		table.WUnlock()
+		// table.RUnlock()
 		return err
 	}
 	defer bucket.page.Put()
 	bucket.WLock()
-	// check not split: unlock table
-	if bucket.numKeys+1 < BUCKETSIZE {
-		table.RUnlock()
-	}
+	// // check split: unlock the read lock and add write lock
+	// if bucket.numKeys+1 >= BUCKETSIZE {
+	// 	table.RUnlock()
+	// 	table.WLock()
+	// }
 
 	split, err := bucket.Insert(key, value)
 	if err != nil {
-		if split {
-			// when split, the table has NOT been unlocked
-			defer table.RUnlock()
-		}
+		// if split {
+		// 	// when split, the table has NOT been unlocked
+		// 	table.WUnlock()
+		// }
 		// unlock the bucket
+		table.WUnlock()
 		bucket.WUnlock()
 		return err
 	}
@@ -192,11 +196,13 @@ func (table *HashTable) Insert(key int64, value int64) error {
 		// at this time, the table has been unlocked
 		// only unlock the bucket
 		bucket.WUnlock()
+		// table.RUnlock()
+		table.WUnlock()
 		return nil
 	}
 
 	// get here, it is the case of split, wanna make sure the recursion is done using defer
-	defer table.RUnlock()
+	defer table.WUnlock()
 	defer bucket.WUnlock()
 	return table.Split(bucket, hash)
 	/* SOLUTION }}} */
